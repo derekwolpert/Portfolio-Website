59b1858c8913ff41c5dfadd16e034a612bb0fab1
Derek Wolpert <dfnj123@gmail.com>
2019-11-16 19:31:44 -0500

    minor adjustments to light/dark mode auto detection

--- assets/js/theme_detect.js
+++ assets/js/theme_detect.js
@@ -1,13 +1,14 @@
 if (window.matchMedia('(prefers-color-scheme: dark)').matches === true) {
     document.getElementById('checkbox').checked = true;
+    document.documentElement.setAttribute('data-theme', 'dark');
 }
 window.matchMedia('(prefers-color-scheme: dark)').addListener((e) => {
     if (e.matches === true) {
         document.getElementById('checkbox').checked = true;
-        document.documentElement.setAttribute('data-theme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
     else if (e.matches === false) {
         document.getElementById('checkbox').checked = false;
-        document.documentElement.setAttribute('data-theme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light');
     }
 });
